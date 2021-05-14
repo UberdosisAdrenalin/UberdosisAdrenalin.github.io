d79b3c98b72e3dc0c1966f14a1124ec1d63f3d8a
dhideo <dhideo@hotmail.com>
2021-05-14 20:00:07 -0300

    o

---
+++ sw.js
@@ -0,0 +1,64 @@
+var cacheName = 'dhideocombr';
+
+var filesToCache = [
+  '/',
+  'index.html',
+  '/assets/js/script.min.js',
+  'manifest.json',
+  '/assets/img/Logo.png',
+  '/assets/img/Logo_16x16.png',
+  '/assets/img/maskable_icon_x192.png',
+  '/assets/img/maskable_icon_x512',
+  '/assets/css/styles.min.css',
+];
+
+// todo: check if service worker is installed before
+
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * 'Install' event. Writing files to browser cache
+ *
+ * @param {string} Event name ('install')
+ * @param {function} Callback function with event data
+ *
+ */
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('sw: writing files into cache');
+      return cache.addAll(filesToCache);
+    })
+  )
+});
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * 'Activate' event. Service worker is activated
+ *
+ * @param {string} Event name ('activate')
+ * @param {function} Callback function with event data
+ *
+ */
+self.addEventListener('activate', function (event) {
+  console.log('sw: service worker ready and activated', event);
+});
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * 'Fetch' event. Browser tries to get resources making a request
+ *
+ * @param {string} Event name ('fetch')
+ * @param {function} Callback function with event data
+ *
+ */
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // test if the request is cached
+    caches.match(event.request).then(function(response) {
+      // 1) if response cached, it will be returned from browser cache
+      // 2) if response not cached, fetch resource from network
+      return response || fetch(event.request);
+    }).catch(function (err) {
+      // if response not cached and network not available an error is thrown => return fallback image
+      return caches.match('/assets/img/Logo.png');
+    })
+  )
+});
